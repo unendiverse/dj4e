3a9967f23dadf4caad2c4d7ae15a54e04982311a
Unendiverse <rahulrakida@gmail.com>
2024-02-29 17:26:23 +0300

    new views

--- django_projects/mysite/polls/urls.py
+++ django_projects/mysite/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /polls/
     path('', views.index, name='index'),
+    # /polls/<QUESTION_ID>
+    path("<int:question_id>/", views.detail, name="detail"),
+    # /polls/<QUESTION_ID>/results
+    path("<int:question_id>/results", views.results, name="results"),
+    # /polls/<QUESTION_ID/vote
+    path("<int:question_id>/vote", views.vote, name="vote"),
 ]
