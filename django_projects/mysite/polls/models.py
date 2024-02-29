f520fd5bf826c3795c5deab84cd6af7575305073
Unendiverse <rahulrakida@gmail.com>
2024-02-29 17:18:03 +0300

    test commit from PC

--- django_projects/mysite/polls/models.py
+++ django_projects/mysite/polls/models.py
@@ -11,6 +11,7 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+        # test comment
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
