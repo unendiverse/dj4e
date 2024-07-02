07a3eee8e658b7ddc6f4587274736faa774e53cd
Rahul Wavare <rahulrakida@gmail.com>
2024-07-02 12:03:18 +0000

    new

--- django_projects/mysite/mysite/urls.py
+++ django_projects/mysite/mysite/urls.py
@@ -2,6 +2,7 @@ import os
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
+from django.views.generic.base import TemplateView
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,4 +15,5 @@ urlpatterns = [
         {'document_root': SITE_ROOT, 'show_indexes': True},
         name='site_path'
     ),
+    path('', TemplateView.as_view(template_name='home/main.html')),
 ]
